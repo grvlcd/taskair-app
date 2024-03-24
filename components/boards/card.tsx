7cdec3bc24718f990b57cbde4f032af29735692c
Christian <generalpride@gmail.com>
2024-03-24 20:37:19 +0800

    added dynamic page for viewing boards

--- components/boards/card.tsx
+++ components/boards/card.tsx
@@ -1,9 +1,18 @@
+import { useRouter } from "next/router";
+
 type TBoardCard = {
+  boardId: number;
   title: string;
   description: string;
 };
 
-const BoardCard = ({ title, description }: TBoardCard) => {
+const BoardCard = ({ boardId, title, description }: TBoardCard) => {
+  const navigate = useRouter();
+
+  const handleViewBoard = () => {
+    navigate.push(`boards/${boardId}`);
+  };
+
   return (
     <div className="rounded-2xl p-8 w-72 ring-1 xl:p-10 bg-gray-900 ring-gray-900">
       <div>
@@ -15,13 +24,12 @@ const BoardCard = ({ title, description }: TBoardCard) => {
         </h3>
       </div>
       <p className="mt-4 text-sm leading-6 text-gray-300">{description}</p>
-      <a
-        href="#"
-        aria-describedby="tier-enterprise"
-        className="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white"
+      <button
+        onClick={handleViewBoard}
+        className="w-full mt-6 rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-indigo-700 text-white hover:bg-white/20 focus-visible:outline-white"
       >
         View Board
-      </a>
+      </button>
     </div>
   );
 };
--- pages/boards/index.tsx
+++ pages/boards/index.tsx
@@ -8,6 +8,7 @@ import { TUser } from "@/lib/models/user/TUser";
 import useBoardStore, { TBoard } from "@/lib/store/boards/boardStore";
 import BoardModal from "@/components/boards/modals/boardModal";
 import BoardCard from "@/components/boards/card";
+import { useRouter } from "next/router";
 
 type BoardProps = {
   user: TUser;
@@ -23,26 +24,25 @@ const BoardsPage: NextPage<BoardProps> = ({ user }: BoardProps) => {
 
   return (
     <DashboardLayout isAuthenticated={isUserAuthenticated}>
-      <div className="p-4 space-y-4">
-        <button
-          type="button"
-          onClick={() => {
-            toggle(true);
-          }}
-          className="rounded-md bg-gray-800 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-        >
-          <p>Create Board</p>
-        </button>
-        <div className="flex flex-row space-x-5">
-          {!isEmpty(data) &&
-            data.shared.map((board: TBoard) => (
-              <BoardCard
-                key={board.id}
-                title={board.name}
-                description={board.description}
-              />
-            ))}
-        </div>
+      <button
+        type="button"
+        onClick={() => {
+          toggle(true);
+        }}
+        className="rounded-md bg-gray-800 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+      >
+        <p>Create Board</p>
+      </button>
+      <div className="flex flex-row space-x-5">
+        {!isEmpty(data) &&
+          data.shared.map((board: TBoard) => (
+            <BoardCard
+              key={board.id}
+              boardId={board.id}
+              title={board.name}
+              description={board.description}
+            />
+          ))}
       </div>
       <BoardModal />
     </DashboardLayout>
